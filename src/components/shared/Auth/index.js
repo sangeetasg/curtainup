b9ac222fde23394d95d734645b9dfcae59ca025c
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2023-06-20 14:33:09 +0100

    Login page component added

--- src/components/shared/Auth/index.js
+++ src/components/shared/Auth/index.js
@@ -1,21 +1,28 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import siteLogo from "../../../assets/images/cu-logo.png";
-import "./auth.css";
+import "./auth.scss";
+import { Card } from "react-bootstrap";
 
 
-const Auth = ({ heading, children}) =>{
+const Auth = ({ authHeading, children}) =>{
   
   return (
     <div className="page-auth d-flex flex-1">
-      <div className="page-auth-wrapper d-flex flex-1">
+      <div className="page-auth-wrapper d-flex flex-1 p-4">
         <div className="page-logo-section">
-          <a href="#" className="auth-logo d-block mx-auto">
+          <div className="auth-logo mx-auto">
             <img src={siteLogo} alt="logo" />
-          </a>
+          </div>
         </div>
         <div className="page-auth-section d-flex flex-1 align-items-center justify-content-center">
-          
+          <Card className="card-auth border-0 bg-transparent flex-1">
+            <Card.Body className="p-0">
+              <Card.Title className="fw-bold mb-0">{authHeading}</Card.Title>
+              <span className="d-block fs-sm mb-5">Enter your details below.</span>
+                {children}
+            </Card.Body>
+          </Card>
         </div>
       </div>
     </div>
@@ -23,7 +30,7 @@ const Auth = ({ heading, children}) =>{
 };
 
 Auth.propTypes = {
-  heading: PropTypes.string.isRequired,
+  authHeading: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired
 };
 
