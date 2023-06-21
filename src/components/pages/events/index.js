c289411c44a1ac711333e476d03fbca6e8aca26d
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2023-06-21 15:05:39 +0100

    Created event page component

---
+++ src/components/pages/events/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../../shared/Header";
+
+const Event = () =>{
+  
+  return (
+    <div className="cu-page-content d-flex flex-1 scroll-y flex-column gap-3 p-4">
+      <Header />
+      <div className="d-flex flex-1 scroll-y">
+        
+      </div>
+    </div>
+  );
+};
+
+// Auth.propTypes = {
+//   authHeading: PropTypes.string.isRequired,
+//   children: PropTypes.node.isRequired
+// };
+
+export default Event;
