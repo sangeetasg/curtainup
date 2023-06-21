8fd7fe10065089980abdcb923dc1421cc33a91b4
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2023-06-21 17:51:38 +0100

    eye icon set in login page

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,7 +1,17 @@
-import { Form , Button} from "react-bootstrap";
+import React, { useState } from 'react';
+import { Form, Button, InputGroup } from "react-bootstrap";
+import { BsEyeFill } from "react-icons/bs";
 import Auth from "../shared/Auth";
 
+
 const Login = () => {
+
+  const [isPasswordAutofilled, setPasswordAutofilled] = useState(true);
+
+  const handlePasswordChange = (event) => {
+    const isAutofilled = event.target.value !== "";
+    setPasswordAutofilled(isAutofilled);
+  };
   return (
     <>
       <Auth authHeading="Sign in">
@@ -12,9 +22,16 @@ const Login = () => {
           </Form.Group>
           <Form.Group controlId="formPlaintextPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control className="p-3 border-0 bg-white" type="password" placeholder="Enter your password" />
+            <InputGroup>
+              <Form.Control className="p-3 border-0 bg-white" type="password" placeholder="Enter your password" onChange={handlePasswordChange}/>
+              {isPasswordAutofilled && (
+                <InputGroup.Text className="border-0 bg-white">
+                  <BsEyeFill className="fs-6" />
+                </InputGroup.Text>
+              )}
+            </InputGroup>
           </Form.Group>
-          <Form.Group className="d-grid pt-3">
+          <Form.Group className="d-grid pt-4">
             <Button variant="primary" type="submit" className="text-uppercase py-3 fw-bold">
               Login
             </Button>
