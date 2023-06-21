980f9ef368323b791c61ec1517a278825a313ee6
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2023-06-21 17:06:02 +0100

    sub header added in event list page

--- src/components/pages/events/index.js
+++ src/components/pages/events/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../shared/Header";
+import SubHeader from "../../shared/SubHeader";
 
 const Event = () =>{
   
   return (
-    <div className="cu-page-content d-flex flex-1 scroll-y flex-column gap-3 p-4">
+    <div className="cu-page-content d-flex flex-1 scroll-y flex-column gap-4 p-4">
       <Header />
+      <SubHeader pageTitle="Events List" pageButton backarrow pageButtonText="Add Event"/>
       <div className="d-flex flex-1 scroll-y">
         
       </div>
---
+++ src/components/shared/SubHeader/index.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import { BiLeftArrowAlt } from "react-icons/bi";
+import "./subHeader.scss";
+import ButtonIcon from '../ButtonIcon';
+import { Button } from 'react-bootstrap';
+
+const SubHeader = ({pageTitle, backarrow,pageButton, pageButtonText }) => {
+
+  return (
+    <div className="cu-subheader d-flex align-items-center justify-content-between">
+      <div className='d-flex align-items-center gap-3'>
+        {backarrow && 
+          <ButtonIcon className="cu-back-btn p-2">
+            <BiLeftArrowAlt className='fs-5'/>
+          </ButtonIcon>
+        }
+        <h1 className="fs-4 fw-semibold mb-0">{pageTitle}</h1>
+      </div>
+      {pageButton && 
+        <Button className='text-uppercase py-3 px-4 fw-bold'>{pageButtonText}</Button>
+      }
+    </div>
+  )
+}
+
+SubHeader.propTypes = {
+  pageTitle: PropTypes.string.isRequired,
+  backarrow: PropTypes.bool,
+  pageButton: PropTypes.bool,
+  pageButtonText: PropTypes.string.isRequired,
+};
+
+export default SubHeader;
