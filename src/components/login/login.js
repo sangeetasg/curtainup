318d4fb804780db842aefe0c47a374f6144730ef
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2023-06-23 09:42:08 +0100

    login page fixes

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -18,18 +18,16 @@ const Login = () => {
         <Form className='d-grid gap-4'>
           <Form.Group controlId="exampleForm.ControlInput1">
             <Form.Label>Email</Form.Label>
-            <Form.Control className="p-3 border-0 bg-white" type="email" placeholder="Enter your email" />
+            <Form.Control className="p-3 bg-white" type="email" placeholder="Enter your email" />
           </Form.Group>
-          <Form.Group controlId="formPlaintextPassword">
+          <Form.Group className={`cu-addon-group ${isPasswordAutofilled ? "input-filled-state" : ""}`}  controlId="formPlaintextPassword">
             <Form.Label>Password</Form.Label>
             <InputGroup>
-              <Form.Control className="p-3 border-0 bg-white" type="password" placeholder="Enter your password" onChange={handlePasswordChange}/>
-              {isPasswordAutofilled && (
-                <InputGroup.Text className="border-0 bg-white">
-                  <BsEyeFill className="fs-6" />
-                </InputGroup.Text>
-              )}
+              <Form.Control className="p-3 bg-white" type="password" placeholder="Enter your password" onChange={handlePasswordChange}/>
             </InputGroup>
+            <div className='form-control-icon'>
+              <BsEyeFill className="fs-6" />
+            </div>
           </Form.Group>
           <Form.Group className="d-grid pt-2">
             <Button variant="primary" type="submit" className="text-uppercase py-3 fw-bold">
